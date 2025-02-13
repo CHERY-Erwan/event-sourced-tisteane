6e0934b9ace98e349ba0bd3026d291617fd95423
CHERY Erwan <chery.erwan@laposte.net>
2025-02-13 18:33:28 +0100

    Alpine & AOS library

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+
+Alpine.start()
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+document.addEventListener('DOMContentLoaded', () => {
+    AOS.init({
+        duration: 1000,
+        easing: 'ease-in-out',
+        once: true,
+    });
+});
